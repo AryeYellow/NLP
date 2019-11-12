e07ff975496ee96d55aa7dcd30d9932470b3462b
洪基伟 <40350987+AryeYellow@users.noreply.github.com>
2019-11-12 17:10:14 +0800

    Add files via upload

--- segment/segment/__init__.py
+++ segment/segment/__init__.py
@@ -1,20 +1,22 @@
 from re import compile
 from math import log
 from segment.corpus import txt2df, df2dt
+
+RE_EN = compile('[a-zA-Z]+')
+RE_NUM = compile('[0-9]+%?|[0-9]+[.][0-9]+%?')
 NA, EN, NUM = 'NA', 'EN', 'NUM'
 GET_FREQ = lambda w: {1: 2000, 2: 300, 3: 40, 4: 5}.get(len(w), 2)
 
 
 class Tokenizer:
-    re_en = compile('[a-zA-Z][a-zA-Z0-9]+')
-    re_num = compile('[0-9]+%?|[0-9]+[.][0-9]+%?')
-    re_temporary = None  # 临时自定义
 
     def __init__(self, word2freq, word2flag):
         self.word2freq = word2freq
         self.total = sum(word2freq.values())
         self.max_len = max(len(i) for i in word2freq.keys())  # 词最大长度
         self.word2flag = word2flag
+        self.re_ls = [RE_EN, RE_NUM]  # 正则表达式匹配
+        self.re_flags = [EN, NUM]
 
     @classmethod
     def initialize(cls):
@@ -33,13 +35,11 @@ class Tokenizer:
                 word = sentence[head: middle]
                 if word in self.word2freq:
                     DAG[head].append(middle - 1)
-                # ------------- 正则 ------------- #
-                elif self.re_en.fullmatch(word):
-                    DAG[head].append(middle - 1)
-                elif self.re_num.fullmatch(word):
-                    DAG[head].append(middle - 1)
-                elif self.re_temporary and self.re_temporary.fullmatch(word):
-                    DAG[head].append(middle - 1)
+                    continue
+                for r in self.re_ls:
+                    if r.fullmatch(word):
+                        DAG[head].append(middle - 1)
+                        break
         return DAG
 
     def calculate(self, sentence):
@@ -67,8 +67,9 @@ class Tokenizer:
     def lcut(self, sentence):
         return list(self.cut(sentence))
 
-    def add_word(self, word, freq=1, flag=None, add=True):
+    def add_word(self, word, freq=-1, flag=None, add=True):
         original_freq = self.word2freq.get(word, 0)
+        freq = GET_FREQ(word) if freq < 0 else freq
         self.word2freq[word] = original_freq + freq if add else freq
         self.total = self.total - original_freq + self.word2freq[word]
         self.word2flag[word] = flag if flag else self.get_flag(word)
@@ -80,18 +81,27 @@ class Tokenizer:
             self.total -= original_freq
             del self.word2flag[word]
 
+    def update_dt(self, word2freq, word2flag):
+        self.word2freq.update(word2freq)
+        self.total = sum(word2freq.values())
+        self.max_len = max(len(i) for i in word2freq.keys())  # 词最大长度
+        self.word2flag = dict(word2flag, **self.word2flag)
+
     def get_flag(self, word):
         if word in self.word2flag:
             return self.word2flag[word]
-        elif self.re_en.fullmatch(word):
-            return EN
-        elif self.re_num.fullmatch(word):
-            return NUM
-        else:
-            return NA
+        for r, flag in zip(self.re_ls, self.re_flags):
+            if r.fullmatch(word):
+                return flag
+        return NA
 
     def get_flags(self, words):
         return [self.get_flag(word) for word in words]
+
+    def add_re(self, re_ls, re_flags, max_len):
+        self.re_ls.extend(re_ls)
+        self.re_flags.extend(re_flags)
+        self.max_len = max_len
 
 
 class Tk(Tokenizer):
@@ -113,19 +123,20 @@ class Tk(Tokenizer):
                 print(sentence[max(0, x-half):x] + red + sentence[y:y+half])
 
     def highlight_re(self, sentence, pattern, half=32):
-        self.re_temporary = compile(pattern) if isinstance(pattern, str) else pattern
+        self.re_ls.append(compile(pattern) if isinstance(pattern, str) else pattern)
         for l_word, x, y in self.cut_with_position(sentence):
-            if self.re_temporary.fullmatch(l_word):
+            if self.re_ls[-1].fullmatch(l_word):
                 word = '\033[031m%s\033[0m' % l_word
                 print(sentence[max(0, x-half):x] + word + sentence[y:y+half])
-        self.re_temporary = None
+        print(self.re_ls.pop())
 
-    def highlight_output(self, sentence, word, length=85):
-        hl = '【%s】' % word
-        half = (length - len(hl)) // 2
-        for l_word, x, y in self.cut_with_position(sentence):
-            if l_word == word:
-                yield (l_word, sentence[max(0, x - half):x] + hl + sentence[y:y + half])
+    def highlight_yield(self, sentence, word_set, wrap='【%s】', length=85):
+        word_set = {word_set} if isinstance(word_set, str) else word_set
+        half = (length - max(len(w) for w in word_set)) // 2 - 1
+        s = sentence.replace('【', ' ').replace('】', ' ')
+        for l_word, x, y in self.cut_with_position(s):
+            if l_word in word_set:
+                yield l_word, s[max(0, x-half):x] + wrap % l_word + s[y:y+half]
 
 
 # 实例化
--- segment/segment/app/word_frequency.py
+++ segment/segment/app/word_frequency.py
@@ -1,5 +1,5 @@
 from collections import Counter
-from re import fullmatch
+from re import compile
 from pandas import DataFrame
 from segment.clean import ngram
 from segment.hmm import tk as tk0
@@ -7,14 +7,14 @@ from segment.hmm import tk as tk1
 from segment.hmm_flag import tk as tk2
 
 _dict = set(tk1.word2flag)
-_pattern = '[a-zA-Z0-9\u4e00-\u9fa5]*[a-zA-Z\u4e00-\u9fa5][a-zA-Z0-9\u4e00-\u9fa5]*'
+fullmatch = compile('[a-zA-Z0-9\u4e00-\u9fa5]*[a-zA-Z\u4e00-\u9fa5][a-zA-Z0-9\u4e00-\u9fa5]*').fullmatch
 
 
 def new_word(texts, dictionary=_dict, fname='new_word.xlsx'):
     """探索新词"""
     c = Counter(
         w for t in texts for s in ngram(t) for w in tk1.cut(s)
-        if w not in dictionary and fullmatch(_pattern, w)).most_common()
+        if w not in dictionary and fullmatch(w)).most_common()
     DataFrame(c, columns=['word', 'freq']).to_excel(fname, index=False)
 
 
@@ -22,11 +22,11 @@ def new_word_flag(texts, dictionary=_dict, fname='new_word_flag.xlsx'):
     """探索新词极其词性"""
     c = Counter(
         (w.word, w.flag) for t in texts for s in ngram(t) for w in tk2.cut(s)
-        if w.word not in dictionary and fullmatch(_pattern, w.word)).most_common()
+        if w.word not in dictionary and fullmatch(w.word)).most_common()
     DataFrame([(i[0], i[1], j) for i, j in c], columns=['word', 'flag', 'freq']).to_excel(fname, index=False)
 
 
 def frequency(texts, fname='frequency.xlsx'):
     """词频统计"""
-    c = Counter(w for t in texts for s in ngram(t) for w in tk0.cut(s) if fullmatch(_pattern, w)).most_common()
+    c = Counter(w for t in texts for s in ngram(t) for w in tk0.cut(s) if fullmatch(w)).most_common()
     DataFrame([(w, tk0.get_flag(w), f) for w, f in c], columns=['word', 'flag', 'freq']).to_excel(fname, index=False)
